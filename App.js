544adedc2b6257ec3aacf165c04ea926e6c021aa
prince-loomba <35420620+prince-loomba@users.noreply.github.com>
2019-06-08 17:40:09 +0530

    Delete items again

--- App.js
+++ App.js
@@ -11,14 +11,7 @@ import { Platform, View } from 'react-native';
 import { arrayExpression } from '@babel/types';
 import Inputting from './Inputting';
 import List from './List';
-const img=[
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/City_of_London_skyline_from_London_City_Hall_-_Sept_2015_-_Crop_Aligned.jpg/1200px-City_of_London_skyline_from_London_City_Hall_-_Sept_2015_-_Crop_Aligned.jpg',
-  'https://images.unsplash.com/photo-1519794206461-cccd885bf209?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
-  'https://media.gettyimages.com/photos/downtown-manhattan-new-york-jersey-city-golden-hour-sunset-picture-id910867946?s=612x612'
-]
-function rand(){
-  return img[Math.floor(Math.random()*img.length)];
-}
+
 export default class App extends Component {
   constructor() {
     super()
@@ -48,8 +41,7 @@ export default class App extends Component {
           addItem={this.addItem}
         />
         <List places={this.state.arr}
-        changeState={this.changeState}
-        image={rand()}/>
+        changeState={this.changeState}/>
       </View>
     );
   }
--- List.js
+++ List.js
@@ -1,6 +1,14 @@
 import React,{Component} from 'react'
 import { Platform, StyleSheet, ScrollView,View } from 'react-native';
 import Listitem from './Listitem';
+const img=[
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/City_of_London_skyline_from_London_City_Hall_-_Sept_2015_-_Crop_Aligned.jpg/1200px-City_of_London_skyline_from_London_City_Hall_-_Sept_2015_-_Crop_Aligned.jpg',
+  'https://images.unsplash.com/photo-1519794206461-cccd885bf209?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+  'https://media.gettyimages.com/photos/downtown-manhattan-new-york-jersey-city-golden-hour-sunset-picture-id910867946?s=612x612'
+]
+function rand(){
+  return img[Math.floor(Math.random()*img.length)];
+}
 export default class List extends Component{
   constructor(props){
     super(props);
@@ -10,7 +18,7 @@ export default class List extends Component{
       <View style={{flex:7,justifyContent:"flex-start"}}>
         <ScrollView style={styles.listing}>
           {this.props.places.map((item) => {
-         return <Listitem name={item} image={this.props.image} changeState={this.props.changeState}/>;
+         return <Listitem name={item} image={rand()} changeState={this.props.changeState}/>;
           })}
         </ScrollView>
         </View>
