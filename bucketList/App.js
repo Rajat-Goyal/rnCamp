66522156e09953bee9a407821d4acd19334c5b08
Rajat-Goyal <rajat1881@gmail.com>
2019-06-08 18:00:36 +0530

    split into components

--- bucketList/App.js
+++ bucketList/App.js
@@ -7,59 +7,39 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, View, TextInput, Button, Text } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import {
+  Platform,
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Text
+} from "react-native";
+import ListItem from "./components/ListItem";
+import ListContainer from "./components/ListContainer"
+import InputContainer from "./components/InputContainer";
 
 export default class App extends Component {
   state = {
-    placeName: "",
     placeList: []
   };
-  onChangInput = val => {
-    // if(this.state.placeName.trim() === '')
-    //   return;
+
+  placeAddHandler = ( x ) => {
     this.setState(prevState => {
       return {
-        placeName: val.toLowerCase()
+        placeList: prevState.placeList.concat({
+          placeName: x,
+          placeImage: null
+        })
       };
-    });
-  };
-
-  placeAddHandler = () => {
-    //if (this.state.placeName.trim() === "" ) return;
-
-    this.setState(prevState => {
-      return {
-        placeList: prevState.placeList.concat(this.state.placeName)
-      };
-    });
-
+    }, () => {x="RAJAT"});
     
   };
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            value={this.state.placeName}
-            onChangeText={val => this.onChangInput(val)}
-            placeholder={"I want to go to .."}
-            autoFocus={true}
-            style={styles.placeInput}
-          />
-          <Button title={"Add"} onPress={this.placeAddHandler} />
-        </View>
-        <View style={styles.placeListContainer} >
-          {this.state.placeList.map(place => {
-            return <Text>{place}</Text>
-          })}
-        </View>
+        <InputContainer onAdd={this.placeAddHandler} />
+        <ListContainer placeList={this.state.placeList} /> 
       </View>
     );
   }
@@ -68,38 +48,5 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-  inputContainer: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "baseline",
-    flexDirection: "row",
-    width: "100%",
-    padding: 40
-  },
-  placeListContainer: {
-    flex: 4,
-    backgroundColor: "grey"
-  },
-  container1: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "green"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  },
-  placeInput: {
-    borderBottomColor: "blue",
-    borderBottomWidth: 2,
-    width: "70%"
   }
 });
---
+++ bucketList/components/ListContainer/ListContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import ListItem from "../ListItem";
+
+
+export default ListContainer = (props) => { 
+    return (
+        <View style={styles.placeListContainer}>
+          {props.placeList.map((place, idx) => {
+            return (
+              <ListItem
+                key={idx}
+                placeName={place.placeName}
+                placeImage={place.placeImage}
+              />
+            );
+          })}
+        </View>
+    ); 
+}
+
+const styles= StyleSheet.create({
+    placeListContainer: {
+        flex:7,
+        justifyContent:"flex-start",
+        flexDirection:"column"
+    }
+})
+
